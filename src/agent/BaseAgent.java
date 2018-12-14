89f5f70e958cf0d063ae2e20755ebb2582617244
Ali Mirlou <alimirlou@gmail.com>
2018-12-15 00:58:20 +0330

    Dummy commit

--- src/agent/BaseAgent.java
+++ src/agent/BaseAgent.java
@@ -11,27 +11,27 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public abstract class BaseAgent {
-	Socket socket;
+	private Socket socket;
 	private PrintWriter pw;
 	private Scanner sc;
 	private String username;
 	private boolean player = false;
-	int side;
-	int board[][];
+	private int side;
+	private int board[][];
 
 	BaseAgent() throws IOException {
 		socket = new Socket("localhost", 10000);
 		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
 		sc = new Scanner(socket.getInputStream());
-		authentication();
+		authenticate();
 	}
 
-	void authentication() {
+	void authenticate() {
 		do {
 			setUsername();
 			send(ClientMessage.agent_authentication);
 			send(username);
-		} while (receive() == ServerMessage.authentication_aproval);
+		} while (receive().equals(ServerMessage.authentication_aproval));
 	}
 
 	boolean changeAgentType() {
@@ -42,11 +42,8 @@ public abstract class BaseAgent {
 		} else
 			type = "observer";
 		send(ClientMessage.agent_type + " " + type);
-		if (receive() == ServerMessage.change_agent_type_aproval)
-			return true;
-		return false;
+		return receive().equals(ServerMessage.change_agent_type_aproval);
 	}
-
 
 	// to set followings using UIs depends on agent types: ai / (textual/graphical) human / (textual/graphical) observer
 	abstract void setUsername();
@@ -60,23 +57,19 @@ public abstract class BaseAgent {
 	abstract void leaveGame();
 
 	void start() {
-		new Thread(() -> {
-			this.observe();
-		}).start();
+		new Thread(this::observe).start();
 	}
 
 	private void observe() {
 		while (true) {
-			if (receive() == ServerMessage.world_broadcast) {
+			if (receive().equals(ServerMessage.world_broadcast)) {
 				board = boardParser(receive());
-			} else if (receive() == ServerMessage.turn_broadcast) {
+			} else if (receive().equals(ServerMessage.turn_broadcast)) {
 				String turn = receive();
-				if (turn == "Finish")
+				if (turn.equals("Finish"))
 					break;
-				else if (turn == username) {
-					new Thread(() -> {
-						this.makeDecision();
-					}).start();
+				else if (turn.equals(username)) {
+					new Thread(this::makeDecision).start();
 				}
 			}
 		}
@@ -87,24 +80,23 @@ public abstract class BaseAgent {
 	private int[][] boardParser(String receive) {
 		int tmp[][] = new int[11][9];
 		String[] lines = receive.split("\n");
-		for (int i = 0; i < lines.length; i++) {
+		for (int i = 0; i < lines.length; ++i) {
 			String[] line = lines[i].split(" ");
-			for (int j = 0; j < line.length; j++)
+			for (int j = 0; j < line.length; ++j)
 				tmp[i][j] = Integer.parseInt(line[j]);
 		}
 		return tmp;
 	}
 
-
 	boolean isPlayer() {
 		return player;
 	}
 
-	void send(String s) {
+	private void send(String s) {
 		pw.println(s);
 	}
 
-	String receive() {
+	private String receive() {
 		return sc.nextLine();
 	}
 }
