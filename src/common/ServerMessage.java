0e801a5a97a224251311bc65a91a68ad4a3b6bc9
Ali Aarefi <aliaarefi1997@gmail.com>
2018-12-14 23:56:20 +0330

    Updated common messages

--- src/common/ServerMessage.java
+++ src/common/ServerMessage.java
@@ -1,8 +1,9 @@
 package common;
 
 public class ServerMessage {
-	public static final String error_players_full = "S1";
-	public static final String world_broadcast = "S2";
-	public static final String turn_broadcast = "S3";
-	public static final String authentication_aproval = "S4";
+	public static final String error_players_full = "S0";
+	public static final String world_broadcast = "S1";
+	public static final String turn_broadcast = "S2";
+	public static final String authentication_aproval = "S3";
+	public static final String change_agent_type_aproval = "S4";
 }
