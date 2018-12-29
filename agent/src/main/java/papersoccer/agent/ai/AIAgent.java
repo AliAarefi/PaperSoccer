47a874353496bb53a85850559832b0a174e943de
Ali Mirlou <alimirlou@gmail.com>
2018-12-29 19:27:21 +0330

    Added minimax in partial

--- agent/src/main/java/papersoccer/agent/ai/AIAgent.java
+++ agent/src/main/java/papersoccer/agent/ai/AIAgent.java
@@ -21,17 +21,41 @@ public class AIAgent {
 		}
 	}
 
+	private int[] getValidMoves() {
+		int[][] board = api.board;
+		// TODO
+	}
+
 	private int calculateHeuristic() {
 		int width = api.board.length, height = api.board[0].length, ballPosition = api.ballPosition;
 		if (api.side == 2) return (height - (ballPosition / width)) + Math.abs(width - (ballPosition % width));
 		else return (ballPosition / width) + Math.abs(width - (ballPosition % width));
 	}
 
+	private int max(int depth) {
+		int heuristic = calculateHeuristic();
+		if (depth == 0 || heuristic <= Integer.MIN_VALUE || heuristic >= Integer.MAX_VALUE) {
+			// TODO return
+		}
+
+		int result = Integer.MIN_VALUE;
+		// TODO iterate over valid actions
+		return result;
+	}
+
+	private int min(int depth) {
+		int heuristic = calculateHeuristic();
+		// TODO same as max
+		return 0;
+	}
+
+	private int minimax(int depth) {
+		return max(depth);
+	}
+
 	private void act() {
-//		int heuristic = calculateHeuristic();
+		int dest = 1;  // TODO call minimax and find ball position
 
-		// TODO
-
-//		api.setDecision();
+		api.setDecision(api.ballPosition, dest);
 	}
 }
