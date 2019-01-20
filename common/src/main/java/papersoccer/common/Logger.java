18be709896be8b7672f652985cb10847c3a14968
Ali Mirlou <alimirlou@gmail.com>
2019-01-20 17:11:10 +0330

    Improved logger

--- common/src/main/java/papersoccer/common/Logger.java
+++ common/src/main/java/papersoccer/common/Logger.java
@@ -1,16 +1,45 @@
 package papersoccer.common;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class Logger {
 	private String namespace;
+	private OutputStream out;
+	private int level = LoggerLevel.DEBUG;
 
 	public Logger(String namespace) {
+		this.out = System.out;
+		setNamespace(namespace);
+	}
+
+	public Logger(String namespace, OutputStream out) {
+		this.out = out;
+		setNamespace(namespace);
+	}
+
+	public void setNamespace(String namespace) {
 		this.namespace = namespace;
 	}
 
+	public boolean isLoggable(int level) {
+		return level >= this.level;
+	}
+
+	public void debug(int indent, String message) {
+		if (isLoggable(LoggerLevel.DEBUG)) {
+			try {
+				out.write(String.format("[%s] ", namespace).getBytes());
+				while (indent-- != 0)
+					out.write('\t');
+				out.write(message.getBytes());
+				out.write('\n');
+			} catch (IOException ignored) {
+			}
+		}
+	}
+
 	public void d(int indent, String message) {
-		System.out.print(String.format("[%s] ", namespace));
-		while (indent-- != 0)
-			System.out.print('\t');
-		System.out.println(message);
+		debug(indent, message);
 	}
 }
---
+++ common/src/main/java/papersoccer/common/LoggerLevel.java
@@ -0,0 +1,5 @@
+package papersoccer.common;
+
+public class LoggerLevel {
+	public static final int DEBUG = Integer.MIN_VALUE;
+}
