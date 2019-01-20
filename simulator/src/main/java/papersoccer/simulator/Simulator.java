0306d29a3ab145abfc12a8ff2b32d076cb58db48
Ali Mirlou <alimirlou@gmail.com>
2019-01-20 17:12:13 +0330

    Cleaned code and Improved logging of simulator

--- simulator/src/main/java/papersoccer/simulator/Simulator.java
+++ simulator/src/main/java/papersoccer/simulator/Simulator.java
@@ -17,9 +17,10 @@ class Simulator {
 	private SimulatorWebSocketServer serverWeb;
 	private Environment environment;
 	private Map<UUID, Agent> agents;
-	private UUID players[];
+	private UUID[] players;
 	private Watchable<Boolean> gameReady;
 	private boolean simulating = true;
+	private boolean gameRunning = false;
 	private Thread messageHandler;
 	private String turn;
 
@@ -46,7 +47,7 @@ class Simulator {
 			try {
 				while (simulating) {
 					Message m = MessageQueue.getInstance().take();
-					log.d(0, String.format("New message received from agent %s", agents.get(m.id).username));
+					log.d(0, String.format("New message received from agent %s", m.id.toString()));
 					handle(m);
 				}
 			} catch (InterruptedException e) {
@@ -79,10 +80,12 @@ class Simulator {
 
 	void setEnvironment(Environment e) {
 		environment = e;
+		environment.clear();
 	}
 
 	private void start() {
 		environment.clear();
+		gameRunning = true;
 	}
 
 	void startWhenReady() {
@@ -105,20 +108,23 @@ class Simulator {
 					// TODO check username uniqueness
 					agent.username = message[1];
 					agent.send(ServerMessage.authentication_approved);
-					agent.send(ServerMessage.world_broadcast);
-					agent.send(environment.convertToString());
-					agent.send(ServerMessage.ball_position_broadcast);
-					agent.send(Integer.toString(environment.getBallPosition()));
+					log.d(0, String.format("Agent %s successfully authenticated.", agent.id.toString()));
+					if (gameRunning) {
+						agent.send(ServerMessage.world_broadcast);
+						agent.send(environment.convertToString());
+						agent.send(ServerMessage.ball_position_broadcast);
+						agent.send(Integer.toString(environment.getBallPosition()));
+					}
 				} else agent.send(ServerMessage.authentication_failed);
 				break;
 
 			case ClientMessage.join_request:
-				if (message.length == 2) {
+				if (gameRunning && message.length == 2) {
 					int side = Integer.parseInt(message[1]);
 					if (players[side] == null) {
 						agent.setPlayer(side);
 						players[side] = agent.id;
-						log.d(0, String.format("Player %d is set.", agent.side));
+						log.d(0, String.format("Agent %s is set to player %d.", agent.id.toString(), agent.side));
 						checkGameReady();
 					} else {
 						if (players[players.length - 1 - side] == null)
@@ -130,8 +136,9 @@ class Simulator {
 				break;
 
 			case ClientMessage.leave_request:
-				if (true) {
+				if (gameRunning) {
 					agent.send(ServerMessage.leave_accepted);
+					log.d(0, String.format("Agent %s left the game.", agent.id.toString()));
 				} else {
 					agent.send(ServerMessage.leave_failed);
 				}
@@ -141,8 +148,10 @@ class Simulator {
 				if (message.length == 3) {
 					if (environment.doAction(Integer.parseInt(message[1]), Integer.parseInt(message[2]))) {
 						agent.send(ServerMessage.action_accepted);
-						// broadcast world & ballPosition & turn
+						log.d(0, String.format("Agent %s acted.", agent.id.toString()));
 						turn = whoseTurnIsIt(environment.getTurn());
+
+						// Broadcast world, ballPosition & turn
 						for (Agent ag : agents.values()) {
 							ag.send(ServerMessage.world_broadcast);
 							ag.send(environment.convertToString());
